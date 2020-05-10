35761756c260c3fdee92e0b34f397b20c686728c
codeyhj <colgateyhj@gmail.com>
2020-05-10 21:22:53 +0800

    build(webpack): webpack生产环境输出位置

--- config/webpack/webpack.config.pro.ts
+++ config/webpack/webpack.config.pro.ts
@@ -6,17 +6,24 @@ import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 import baseConfig from "./webpack.config.base";
 
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
 import path from "path";
 
 const config: webpack.Configuration = {
-    output: {
-        path: path.resolve(__dirname, '../../dist'),
-        filename: "[name].[chunkhash].js",
-        chunkFilename: "[name].js"
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
-}
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    filename: "js/[name].[chunkhash].js",
+    chunkFilename: "[name].js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 提取 css
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+      chunkFilename: "css/[name].[contenthash].css",
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+};
 
-module.exports = webpackMerge(baseConfig, config)
+module.exports = webpackMerge(baseConfig, config);
